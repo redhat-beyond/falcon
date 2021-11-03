78cb7b57e2dbf13ee254f182eaec866038d029a3
Or Zionpour <orzionpour@gmail.com>
2021-11-03 19:16:48 +0200

    Added HTML login page
    [-] Created static folder - will contain any 'static' related files such as images, videos, text files, etc..
    [-] Add falcon logo to static folder.
    [-] Created templates folder.
    [-] Created login.html file - display falcon's initial login form.
    [-] Change layout using css
    [-] Add CSS design

--- falcon/settings.py
+++ falcon/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'falcon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
