e9815d315e6809d7096f0658536c6eb0835d2a2e
Or Zionpour <orzionpour@gmail.com>
2021-11-03 19:16:48 +0200

    Added HTML login page
    [-] Created static folder - will contain any 'static' related files such as images, videos, text files, etc..
    [-] Add falcon logo to static folder.
    [-] Created templates folder.
    [-] Created login.html file - display falcon's initial login form.
    [-] Change layout using css
    [-] Add CSS design
    Resolve #9.

--- falcon/urls.py
+++ falcon/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from falcon import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login ,name='login')
 ]
