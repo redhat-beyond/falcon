351a74b4b88412347289cb70923a20edfdb147f0
avraham1992 <estyk4@gmail.com>
2021-11-10 16:21:41 +0200

    add apps

--- falcon/settings.py
+++ falcon/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Employees.apps.EmployeesConfig',
+    'Manager.apps.ManagerConfig',
+    'Task.apps.TaskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
