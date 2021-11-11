9a340dbb31af9205fdd00178be62271736120f92
avraham1992 <estyk4@gmail.com>
2021-11-11 15:54:42 +0200

    add migra file

--- Task/models.py
+++ Task/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.db.models.manager import Manager
-
+from django.db.models.deletion import CASCADE
+from Manager.models import Manager
 
 class Task(models.Model):
     author = models.ForeignKey(Manager, on_delete=models.RESTRICT)
     title = models.CharField(max_length=50)
-    name_task = models.CharField()
+    name_task = models.CharField(max_length=50)
     about = models.TextField(blank=True)
     status = models.CharField(max_length=50)
     publish_date = models.DateTimeField()
