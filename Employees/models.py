915298b894559fdb18fac3ebe4a95504a0ecd4ef
avraham1992 <estyk4@gmail.com>
2021-11-11 10:54:45 +0200

    add models

--- Employees/models.py
+++ Employees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.manager import Manager
 
-# Create your models here.
+class Employees(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employer = models.ManyToOneField(Manager)
+    email = models.EmailField(unique=True)
+    employer = models.ForeignKey(Manager)
+    about = models.TextField(blank=True)
+
--- Task/models.py
+++ Task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.manager import Manager
 
-# Create your models here.
+
+class Task(models.Model):
+    author = models.ForeignKey(Manager, on_delete=models.RESTRICT)
+    manager = models.ManyToOneField(Manager,  primary_key=True)
+    title = models.CharField(max_length=50)
+    name_task = models.CharField()
+    about = models.TextField(blank=True)
+    status = models.CharField(max_length=50)
+    publish_date = models.DateTimeField()
+    property = models.IntegerField()
+   
+
+    
+    
+    
+
