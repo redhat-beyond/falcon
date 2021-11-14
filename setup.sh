7d871f8d7c253ab91b2a66f06748e60a85f5040e
orzionpour <orzionpour@gmail.com>
2021-11-14 14:14:54 +0200

    Add pytest to project

--- setup.sh
+++ setup.sh
@@ -12,3 +12,4 @@ pipenv run python manage.py migrate
 # run our app. Nohup and “&” are used to let the setup script finish
 # while our app stays up. The app logs will be collected in nohup.out
 nohup pipenv run python manage.py runserver 0.0.0.0:8000 &
+
