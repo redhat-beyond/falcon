b36ae1c3a0a82604d48958d6cf77637c7f701333
Aviv Liberman <avivliberman@gmail.com>
2021-11-22 23:25:37 +0200

    Refactor user and task tests
    - Change tests to pytest
    - Removed dummy test
    - Create factory for tests

--- users/tests.py
+++ users/tests.py
@@ -1,30 +1,11 @@
-from django.test import TestCase
 from users.models import Role, User, Team
 
 
-class TestUserModel(TestCase):
-
-    def test_create_user(self):
-        username = "username"
-        password = "password"
-        email = "user@example.com"
-        first_name = "first_name"
-        last_name = "last_name"
-        name = "Alpha"
-        description = "best team ever"
-        team = Team.objects.create(name=name, description=description)
-        user = User.create_user(
-            username, email, password, first_name, last_name, Role.EMPLOYEE, team)
-        assert isinstance(user, User)
-        assert user.user.first_name == first_name
+def test_create_user(employee_1):
+    assert isinstance(employee_1, User)
+    assert employee_1.role == Role.EMPLOYEE
 
 
-class TestTeamModel(TestCase):
-
-    def test_create_team(self):
-        name = "Alpha"
-        description = "best team ever"
-        team = Team.objects.create(name=name, description=description)
-        assert isinstance(team, Team)
-        assert team.name == name
-        assert team.description == description
+def test_create_team(team_1):
+    assert isinstance(team_1, Team)
+    assert team_1.name == "Team1"
