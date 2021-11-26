cc2dd5b0d07cd410b653255aa83b5ff983ca9b00
avraham1992 <estyk4@gmail.com>
2021-11-27 01:30:56 +0200

    up

--- falcon/settings.py
+++ falcon/settings.py
@@ -29,9 +29,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Employees.apps.EmployeesConfig',
-    'Manager.apps.ManagerConfig',
-    'Task.apps.TaskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
