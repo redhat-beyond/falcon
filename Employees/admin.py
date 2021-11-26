2fefe4e4d00e3359e9e2d08c3b9fd5e42cd947fe
avraham_chababo <63585549+avraham1992@users.noreply.github.com>
2021-11-27 01:07:28 +0200

    Delete Employees directory

--- Employees/admin.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-#from django.contrib import admin
-=======
-from django.contrib import admin
->>>>>>> working_branch
-
-# Register your models here.
--- Employees/tests.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-#from django.test import TestCase
-=======
-from django.test import TestCase
->>>>>>> working_branch
-
-# Create your tests here.
