b05b0133885c9f32cba5cd2af920c9c18f5d56f5
Aviv Liberman <avivliberman@gmail.com>
2021-11-26 12:33:07 +0200

    fix flake8

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from enumchoicefield import ChoiceEnum, EnumChoiceField
-from django.conf import settings
 from django.contrib.auth.models import User as DjangoUser
 
 
