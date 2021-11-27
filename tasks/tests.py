57025ed0fb73d14906c50d74299021e155a0e8d8
Asafh7 <92928891+Asafh7@users.noreply.github.com>
2021-11-27 22:05:04 +0200

    update status/priority and tests (#84)
    * Added updates impl
    
    * Update tests.py
    
    adding Aviv's comments
    
    * adding tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -68,3 +68,13 @@ class TestComments:
             Comment.objects.create(appUser=employee_1,
                                    task=task_1,
                                    description="Need more info")
+
+    def test_update_status(self, task_1):
+        task_1.update_status(Status.DONE)
+        assert task_1.status == Status.DONE
+        assert Task.objects.get(pk=task_1.id).status == Status.DONE
+
+    def test_update_priority(self, task_1):
+        task_1.update_priority(Priority.LOW)
+        assert task_1.priority == Priority.LOW
+        assert Task.objects.get(pk=task_1.id).priority == Priority.LOW
