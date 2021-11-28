51193e506b79051e15d5b38609ed61f16d5816a3
avraham_chababo <63585549+avraham1992@users.noreply.github.com>
2021-11-28 18:02:01 +0200

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField() 
+    email = forms.EmailField()
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
