5bd4fcb350f86f57d02d3d1b8a66839439eb0a54
avraham1992 <estyk4@gmail.com>
2021-12-02 00:54:24 +0200

    fix-home

--- falcon/views.py
+++ falcon/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from tasks.models import Task
 
 
 def homepage(request):
-    context = {
-      'tasks': Task.objects.all()
-      }
-    return render(request, 'homepage.html', context)
+    return render(request, 'homepage.html')
