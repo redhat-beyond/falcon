8969c698e6e9cb6881994d0565e40e2908d19655
Aviv Liberman <avivliberman@gmail.com>
2021-12-04 19:06:54 +0200

    Fixed urls and redirects

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,4 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('homepage/', auth_views.LoginView.as_view(template_name='homepage.html'), name='homepage'),
 ]
