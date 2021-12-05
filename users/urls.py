961c7b6b56d05618cca3c5f77d9acfaa610d0f42
Aviv Liberman <avivliberman@gmail.com>
2021-12-05 20:15:56 +0200

    fix login redirect

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from users import views
 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ]
