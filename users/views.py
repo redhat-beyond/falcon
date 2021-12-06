175428d13487e2cbbbb23c27a256a89ab0e2a5fd
Aviv Liberman <45610501+AvivLiberman@users.noreply.github.com>
2021-12-06 15:05:17 +0200

    Fixed urls and redirects (#107)
    * Fixed urls and redirects
    
    * Changed if statements to be more readable
    
    * fix login redirect
    
    * fix flake 8

--- users/views.py
+++ users/views.py
@@ -1,1 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
+
+
+def login(request):
+    if(request.user.is_authenticated):
+        return redirect('/')
+    else:
+        return auth_views.LoginView.as_view(template_name='users/login.html')(request)
+
+
+def logout(request):
+    return auth_views.LogoutView.as_view(template_name='users/logout.html')(request)
