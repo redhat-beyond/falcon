f70355a906a751418a51cd14684aafcb79793866
orzionpour <orzionpour@gmail.com>
2021-12-08 12:21:22 +0200

    Refactor PR #105 (#111)
    - Remove 'default=None' from name field on Team model.
    - Remove 'default=None' in name field from Team model migration file.
    - Add transaction=True to be able to use database operation after an exception catch.
    - Change test_create_team_without_title by giving the creating method a blank title (which we expect to raise an exception).

--- users/tests/test_create_teams.py
+++ users/tests/test_create_teams.py
@@ -12,4 +12,13 @@ class TestTeams:
 
     def test_create_team_without_title(self):
         with pytest.raises(Exception):
-            Team.objects.create(description="Best team ever")
+            Team.objects.create(title="", description="Best team ever")
+
+    @pytest.mark.django_db(transaction=True)
+    def test_create_team_with_same_name(self):
+        assert len(Team.objects.all()) == 0
+        Team.objects.create(name="Team1", description="This team will be created")
+        assert len(Team.objects.all()) == 1
+        with pytest.raises(Exception):
+            Team.objects.create(name="Team1", description="This team will be created")
+        assert len(Team.objects.all()) == 1, "Team1 was created twice"
