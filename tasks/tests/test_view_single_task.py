6e8311edae25d17908293b9de99398f76e8b05d4
Aviv Liberman <avivliberman@gmail.com>
2021-12-12 23:19:00 +0200

    fix test_view_single_task with the new ViewTaskForm

--- tasks/tests/test_view_single_task.py
+++ tasks/tests/test_view_single_task.py
@@ -1,5 +1,5 @@
 from tasks.models import Status, Task
-from tasks.forms import TaskForm
+from tasks.forms import ViewTaskForm
 import pytest
 
 
@@ -20,7 +20,7 @@ class TestViewSingleTask:
     def test_view_task_form(self, client, task_1, employee_1):
         client.login(username=employee_1.user.username, password='password')
         response = client.get(f'/tasks/{task_1.id}')
-        assert isinstance(response.context.get('form'), TaskForm)
+        assert isinstance(response.context.get('form'), ViewTaskForm)
 
     def test_change_status(self, client, task_1, employee_1):
         client.login(username=employee_1.user.username, password='password')
