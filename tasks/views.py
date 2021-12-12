df0cfab7bef5e0c16f8f87847e012e310627a70a
Arad Zilberstein <62508697+aradz1997@users.noreply.github.com>
2021-12-12 20:29:07 +0200

    fixing isuues

--- tasks/views.py
+++ tasks/views.py
@@ -16,31 +16,40 @@ def view_tasks(request):
 
 
 def new_task(request):
-    if request.method == "POST":
-        form = TaskForm(request.user.id, request.POST)
-        taskForm = form.save(commit=False)
-        if form.is_valid():
-            try:
-                Task.create_task(taskForm.title, taskForm.created_by, taskForm.assignee, taskForm.priority,
-                                 taskForm.status, taskForm.description)
-
-                messages.success(request, 'Task was added successfully')
+    context = {'manager': False}
+    if not request.user.is_anonymous:
+        logged_user = User.objects.get(user=request.user)
+        if logged_user is not None:
+            context = {'user': logged_user}
+        if logged_user.is_manager():
+            color = 'red'
+            if request.method == "POST":
+                form = TaskForm(request.user.id, request.POST)
+                taskForm = form.save(commit=False)
+                if form.is_valid():
+                    try:
+                        Task.create_task(taskForm.title, taskForm.assignee, taskForm.created_by, taskForm.priority,
+                                         taskForm.status, taskForm.description)
+                        messages.success(request, 'Task was added successfully')
+                        color = 'green'
+                        form = TaskForm(request.user.id)
+                    except Exception as e:
+                        messages.warning(request, e)
+                        form = TaskForm(request.user.id,
+                                        initial={'title': taskForm.title, 'assignee': taskForm.assignee,
+                                                 'priority': taskForm.priority, 'status': taskForm.status,
+                                                 'description': taskForm.description})
+                else:
+                    messages.warning(request, 'something went wrong')
+                    form = TaskForm(request.user.id,
+                                    initial={'title': taskForm.title, 'priority': taskForm.priority,
+                                             'status': taskForm.status,
+                                             'description': taskForm.description, 'assignee': taskForm.assignee, })
+            else:
                 form = TaskForm(request.user.id)
-            except Exception as e:
-                messages.warning(request, e)
-                form = TaskForm(request.user.id,
-                                initial={'title': taskForm.title, 'assignee': taskForm.assignee,
-                                         'priority': taskForm.priority, 'status': taskForm.status,
-                                         'description': taskForm.description})
-        else:
-            messages.warning(request, 'something went wrong')
-            form = TaskForm(request.user.id,
-                            initial={'title': taskForm.title, 'priority': taskForm.priority, 'status': taskForm.status,
-                                     'description': taskForm.description, 'assignee': taskForm.assignee, })
-    else:
-        form = TaskForm(request.user.id)
+            context = {'form': form, 'user': logged_user, 'color': color, 'manager': True}
 
-    return render(request, 'new_task.html', {'form': form})
+    return render(request, 'new_task.html', context)
 
 
 def view_single_task(request, pk):
@@ -58,3 +67,4 @@ def view_single_task(request, pk):
             redirect(f'tasks/{pk}', {'user': user})
 
     return render(request, 'tasks/view_single_task.html', {'form': form, 'task': task, 'user': user})
+
