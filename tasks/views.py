3379baa37a648ae5a3a42673edb24477113cb3fa
Arad Zilberstein <62508697+aradz1997@users.noreply.github.com>
2021-12-12 13:16:22 +0200

    Created New Task view (#118)
    * Created New Task view
    
    * rebased after merging 110
    
    * fixing CR issues
    
    * adding newline to new_task.html
    
    * fixing flake8

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from tasks.models import Task
-from users.models import Role, User
+from tasks.forms import TaskForm
+from tasks.models import Task, Role, User
+from django.contrib import messages
 
 
 def view_tasks(request):
@@ -12,3 +13,31 @@ def view_tasks(request):
         )
         context['user'] = app_user
     return render(request, 'tasks/tasks.html', context)
+
+
+def new_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.user.id, request.POST)
+        taskForm = form.save(commit=False)
+        if form.is_valid():
+            try:
+                Task.create_task(taskForm.title, taskForm.created_by, taskForm.assignee, taskForm.priority,
+                                 taskForm.status, taskForm.description)
+
+                messages.success(request, 'Task was added successfully')
+                form = TaskForm(request.user.id)
+            except Exception as e:
+                messages.warning(request, e)
+                form = TaskForm(request.user.id,
+                                initial={'title': taskForm.title, 'assignee': taskForm.assignee,
+                                         'priority': taskForm.priority, 'status': taskForm.status,
+                                         'description': taskForm.description})
+        else:
+            messages.warning(request, 'something went wrong')
+            form = TaskForm(request.user.id,
+                            initial={'title': taskForm.title, 'priority': taskForm.priority, 'status': taskForm.status,
+                                     'description': taskForm.description, 'assignee': taskForm.assignee, })
+    else:
+        form = TaskForm(request.user.id)
+
+    return render(request, 'new_task.html', {'form': form})
